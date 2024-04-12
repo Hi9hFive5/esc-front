68d76d11a2231e280130e6246a6c2e40c5a53be8
t4e1 <jtw5738@naver.com>
2024-04-12 17:52:23 +0900

    Feat: 채팅 관련 추가

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import router from '../src/router/router.js'
+import socket from 'vue3-websocket'
 
 const app = createApp(App);
 
 app.use(router);
+app.use(socket, "ws://localhost:8080/myHandler")
 app.mount('#app');
 app.provide('$axios', axios);
 
