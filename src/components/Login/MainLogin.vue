458505d7f369053386bf02008249d149ec0d99e5
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-04-13 18:41:01 +0900

    Feat: 로그인 후에 나오는 페이지 추가

---
+++ src/components/Login/MainLogin.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>로그인 성공!</p>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const token = ref(localStorage.getItem('token'));
+console.log(token.value);
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/MainLogin.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="wrapper">
+        <section>
+            <login></login>
+        </section>
+    </div>
+</template>
+
+<script setup>
+    import login from '../components/Login/MainLogin.vue';
+</script>
+
+<style scoped>
+
+</style>
