fe6418cf33fc4af2f4ebb0fe2ad96b8355296593
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-04-13 18:38:51 +0900

    Feat: 아이디 찾기 페이지 추가
    닉네임과 이름을 통해 아이디를 찾고 결과를 반환해주는 페이지 추가

---
+++ src/views/FindIdPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="wrapper">
+        <section>
+            <findId></findId>
+        </section>
+    </div>
+</template>
+
+<script setup>
+    import findId from '../components/Login/FindId.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/FindIdResultPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="wrapper">
+        <section>
+            <findIdResult></findIdResult>
+        </section>
+    </div>
+</template>
+
+<script setup>
+    import findIdResult from '../components/Login/FindIdResult.vue';
+</script>
+
+<style scoped>
+
+</style>
