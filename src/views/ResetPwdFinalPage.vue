55d1a6ad030c20cc833f961361d726693fe4bda6
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-04-13 18:42:03 +0900

    Feat: 비밀번호 재설정 페이지 추가
    아이디와 이메일을 통해 회원임을 검증하고, 비밀번호를 재설정하는 페이지 추가

---
+++ src/views/ResetPwdFinalPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="wrapper">
+        <section>
+            <ResetPwdFinal></ResetPwdFinal>
+        </section>
+    </div>
+</template>
+
+<script setup>
+    import ResetPwdFinal from '../components/Login/ResetPwdFinal.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ResetPwdPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="wrapper">
+        <section>
+            <ResetPwd></ResetPwd>
+        </section>
+    </div>
+</template>
+
+<script setup>
+    import ResetPwd from '../components/Login/ResetPwd.vue';
+</script>
+
+<style scoped>
+
+</style>
